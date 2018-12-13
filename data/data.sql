5e93b7934b81c1b518d75309f7142698fffdeb91
admin <admin@iMac3.local>
2018-12-13 17:25:32 +0100

    add data.sql

---
+++ data/data.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: market
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `market`
+--
+
+DROP TABLE IF EXISTS `market`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `market` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` text NOT NULL,
+  `images` text NOT NULL,
+  `quantite` mediumint(9) NOT NULL,
+  `prix` mediumint(9) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `market`
+--
+
+LOCK TABLES `market` WRITE;
+/*!40000 ALTER TABLE `market` DISABLE KEYS */;
+INSERT INTO `market` VALUES (1,'Panier ','https://cuisinesauvage.shop/wp-content/uploads/2017/03/panier.jpg',12,15),(2,'Canard','https://images-na.ssl-images-amazon.com/images/I/617iy88IxuL._SL1106_.jpg',2,9),(3,'Tee shirt ultra SWAG','https://images-na.ssl-images-amazon.com/images/I/61BQCEaRKHL._UX342_.jpg',1,1000),(4,'Superbe Lemurien Droguer ','https://www.terresacree.org/actualites/fichiers/images/2015-01/1421683322-Y3N.jpg',1,1);
+/*!40000 ALTER TABLE `market` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-13 16:23:02
